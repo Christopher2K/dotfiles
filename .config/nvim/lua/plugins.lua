b6c65fedb035384d9336e27b5c63650be847e0aa
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-06-13 15:13:19 -0400

    fix: indent

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -5,8 +5,8 @@ local fn = vim.fn
 -- Check packer installation
 local install_path = fn.stdpath("data") .. "/site/pack/packer/opt/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-    execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
-    execute("packadd packer.nvim")
+	execute("!git clone https://github.com/wbthomason/packer.nvim " .. install_path)
+	execute("packadd packer.nvim")
 end
 
 vim.cmd("packadd packer.nvim")
@@ -15,79 +15,79 @@ local packer = require("packer")
 local util = require("packer.util")
 
 packer.init({
-    package_root = util.join_paths(vim.fn.stdpath("data"), "site", "pack"),
+	package_root = util.join_paths(vim.fn.stdpath("data"), "site", "pack"),
 })
 
 packer.startup(function()
-    -- Packer installer
-    use({ "wbthomason/packer.nvim", opt = true })
+	-- Packer installer
+	use({ "wbthomason/packer.nvim", opt = true })
 
-    -- Color Scheme
-    use("Mofiqul/dracula.nvim")
-    use("kyazdani42/nvim-web-devicons")
+	-- Color Scheme
+	use("Mofiqul/dracula.nvim")
+	use("kyazdani42/nvim-web-devicons")
 
-    -- Status bar
-    use("nvim-lualine/lualine.nvim")
+	-- Status bar
+	use("nvim-lualine/lualine.nvim")
 
-    -- Native LSP
-    use("williamboman/nvim-lsp-installer") -- Auto installer
-    use("neovim/nvim-lspconfig") -- Preconfig for LSPs
+	-- Native LSP
+	use("williamboman/nvim-lsp-installer") -- Auto installer
+	use("neovim/nvim-lspconfig") -- Preconfig for LSPs
 
-    -- Snippets
-    use("L3MON4D3/LuaSnip")
+	-- Snippets
+	use("L3MON4D3/LuaSnip")
 
-    -- Completion
-    use("hrsh7th/nvim-cmp") -- Engine
-    use("hrsh7th/cmp-nvim-lsp") -- LSP completion
-    use("hrsh7th/cmp-buffer") -- buffer completion
-    use("hrsh7th/cmp-path") -- path completion
-    use("saadparwaiz1/cmp_luasnip") -- complex completion
+	-- Completion
+	use("hrsh7th/nvim-cmp") -- Engine
+	use("hrsh7th/cmp-nvim-lsp") -- LSP completion
+	use("hrsh7th/cmp-buffer") -- buffer completion
+	use("hrsh7th/cmp-path") -- path completion
+	use("saadparwaiz1/cmp_luasnip") -- complex completion
 
-    -- Auto Pair
-    use("windwp/nvim-autopairs")
+	-- Auto Pair
+	use("windwp/nvim-autopairs")
 
-    -- Format and linting engine
-    use("jose-elias-alvarez/null-ls.nvim") -- Null LS
+	-- Format and linting engine
+	use("jose-elias-alvarez/null-ls.nvim") -- Null LS
 
-    -- Terminal
-    use({ "akinsho/toggleterm.nvim", tag = "v1.*" })
+	-- Terminal
+	use({ "akinsho/toggleterm.nvim", tag = "v1.*" })
 
-    -- Treesitter
-    use("nvim-treesitter/nvim-treesitter")
-    use("windwp/nvim-ts-autotag") -- Auto tag extension
+	-- Treesitter
+	use("nvim-treesitter/nvim-treesitter")
+	use("windwp/nvim-ts-autotag") -- Auto tag extension
 
-    -- Telescope
-    use({
-        "nvim-telescope/telescope.nvim",
-        requires = {
-            "nvim-lua/plenary.nvim",
-        },
-    })
-    use("nvim-telescope/telescope-file-browser.nvim")
-    use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
-    use("nvim-telescope/telescope-project.nvim")
+	-- Telescope
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+		},
+	})
+	use("nvim-telescope/telescope-file-browser.nvim")
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" })
+	use("nvim-telescope/telescope-project.nvim")
 
-    -- Comment
-    use("numToStr/Comment.nvim")
+	-- Comment
+	use("numToStr/Comment.nvim")
 
-    -- Surround
-    use("ur4ltz/surround.nvim")
+	-- Surround
+	use("ur4ltz/surround.nvim")
 
-    -- Better mappings
-    use("b0o/mapx.nvim")
+	-- Better mappings
+	use("b0o/mapx.nvim")
 
-        -- Kitty Conf Highlighting
-        use 'fladson/vim-kitty'
+	-- Kitty Conf Highlighting
+	use("fladson/vim-kitty")
 
-        -- Auto indent detection
-	use 'Darazaki/indent-o-matic'
+	-- Auto indent detection
+	use("Darazaki/indent-o-matic")
 
-    -- LazyGit integration
-    use("kdheepak/lazygit.nvim")
+	-- LazyGit integration
+	use("kdheepak/lazygit.nvim")
 
-    -- Kitty Conf Highlighting
-    use("fladson/vim-kitty")
+	-- Kitty Conf Highlighting
+	use("fladson/vim-kitty")
 
-    -- Indentation and blank lines indicators
-    use("lukas-reineke/indent-blankline.nvim")
+	-- Indentation and blank lines indicators
+	use("lukas-reineke/indent-blankline.nvim")
 end)
