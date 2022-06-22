98406d6af9d2619e0369e80dd26e833fe8f3a17b
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-06-21 22:19:02 -0400

    feat: prettier formating for svelte

--- .config/nvim/lua/nullls_setup.lua
+++ .config/nvim/lua/nullls_setup.lua
@@ -1,6 +1,8 @@
 require("null-ls").setup({
     sources = {
         require("null-ls").builtins.formatting.stylua,
-        require("null-ls").builtins.formatting.prettier,
+        require("null-ls").builtins.formatting.prettier.with({
+            extra_filetypes = { "svelte" }
+        }),
     },
 })
