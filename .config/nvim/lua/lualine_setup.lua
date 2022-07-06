f66f578a4e7a3e5cf859e4f5622245a56dc43066
Christopher N. KATOYI KABA <me@christopher2k.dev>
2022-07-06 08:53:50 -0400

    feat: change lualine look and feel

--- .config/nvim/lua/lualine_setup.lua
+++ .config/nvim/lua/lualine_setup.lua
@@ -5,19 +5,19 @@ if ok then
         options = {
             icons_enabled = true,
             theme = "catppuccin",
-            component_separators = {left = "", right = ""},
-            section_separators = {left = "", right = ""},
+            component_separators = {left = '', right = ''},
+            section_separators = {left = '', right = ''},
             disabled_filetypes = {},
             always_divide_middle = true,
-            globalstatus = false
+            globalstatus = true 
         },
         sections = {
             lualine_a = {"mode"},
             lualine_b = {"branch", "diff", "diagnostics"},
             lualine_c = {"%F"},
-            lualine_x = {"encoding", "fileformat", "filetype"},
-            lualine_y = {"progress"},
-            lualine_z = {"location"}
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {"fileformat", "filetype"},
         },
         inactive_sections = {
             lualine_a = {},
