c177b360d4358f450b91dc9f110f305dbda39da1
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-10-04 14:25:30 -0400

    fix: disable pair

--- dot_config/nvim/private_lua/private_plugins/pairs.lua
+++ dot_config/nvim/private_lua/private_plugins/pairs.lua
@@ -1,8 +1,8 @@
 return {
-  "echasnovski/mini.pairs",
-  commit = "dfa9f6e2576bb8853be277d96b735af59d9be7c2",
-  enabed = false,
-  version = false,
-  main = "mini.pairs",
-  config = true,
+  -- "echasnovski/mini.pairs",
+  -- commit = "dfa9f6e2576bb8853be277d96b735af59d9be7c2",
+  -- enabed = false,
+  -- version = false,
+  -- main = "mini.pairs",
+  -- config = true,
 }
