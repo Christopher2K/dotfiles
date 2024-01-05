e94dd6d8044bb278c06ebfc89abd11a6babb6a09
Christopher N. KATOYI KABA <me@christopher2k.dev>
2024-01-05 01:56:01 -0500

    feat: add more pickers for pick

--- dot_config/nvim/lua/plugins/mini-pick.lua
+++ dot_config/nvim/lua/plugins/mini-pick.lua
@@ -1,9 +1,18 @@
 return {
   "echasnovski/mini.pick",
   version = "*",
-  keys = {
-    { "<leader>ff", "<cmd>Pick files<cr>",     desc = "Find file" },
-    { "<leader>fg", "<cmd>Pick grep_live<cr>", desc = "Search grep" },
+  dependencies = {
+    { "echasnovski/mini.extra", version = "*" },
   },
-  config = true,
+  keys = {
+    { "<leader>ff", "<cmd>Pick files<cr>",      desc = "Find file" },
+    { "<leader>fg", "<cmd>Pick grep_live<cr>",  desc = "Search grep" },
+    { "<leader>fd", "<cmd>Pick diagnostic<cr>", desc = "Find diagnostics" },
+  },
+  config = function()
+    local pick = require("mini.pick")
+    pick.setup()
+    local extra = require("mini.extra")
+    extra.setup()
+  end,
 }
