7db43289e5a539bfddcad0b181f2f6d4c205abd3
Christopher N. KATOYI KABA <me@christopher2k.dev>
2024-01-08 19:02:03 -0500

    feat: add codestats

---
+++ dot_config/nvim/lua/plugins/codestats-nvim.lua
@@ -0,0 +1,6 @@
+return {
+  "YannickFricke/codestats.nvim",
+  main = "codestats-nvim",
+  lazy = false,
+  config = true,
+}
--- dot_config/nvim/lua/plugins/nvim-lint.lua
+++ dot_config/nvim/lua/plugins/nvim-lint.lua
@@ -4,10 +4,10 @@ return {
   config = function()
     local lint = require("lint")
     lint.linters_by_ft = {
-      javascript = { 'eslint' },
-      javascriptreact = { 'eslint' },
-      typescript = { 'eslint' },
-      typescriptreact = { 'eslint' },
+      javascript = { 'eslint_d' },
+      javascriptreact = { 'eslint_d' },
+      typescript = { 'eslint_d' },
+      typescriptreact = { 'eslint_d' },
     }
 
     vim.api.nvim_create_autocmd({ "TextChanged" }, {
